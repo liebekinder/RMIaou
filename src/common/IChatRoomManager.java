f3e73e94f23a96b4a8d731d0c39e66a7bcb9da9d
Ornicare <antoine.floriano@gmail.com>
2013-10-02 19:17:49 +0200

    ICahtRoomManager

---
+++ src/common/IChatRoomManager.java
@@ -0,0 +1,38 @@
+package common;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Client interface for chatroom management
+ * 
+ * @author Ornicare
+ *
+ */
+public interface IChatRoomManager extends Remote {
+	/**
+	 * Return the list of chatroom name's
+	 * @return
+	 * @throws RemoteException
+	 */
+	public String getChatRoomsList() throws RemoteException;
+	
+	/**
+	 * Create a chatroom by using <code>name</code>
+	 * 
+	 * @param name The name of the chatroom that will be created
+	 * @return A String that indicate if the process is successful or not
+	 * @throws RemoteException
+	 */
+	public String createChatRoom(String name) throws RemoteException;
+	
+	/**
+	 * Delete  a chatroom by using <code>name</code>
+	 * 
+	 * @param name The name of the chatroom to delete
+	 * @return A String that indicate if the process is successful or not
+	 * @throws RemoteException
+	 */
+	//TODO extension
+//	public String deleteChatRoom(String name) throws RemoteException;
+}
